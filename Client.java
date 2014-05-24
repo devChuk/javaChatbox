381312dc887f4d7d9c0f97c13384e8bc38df3747
Brian Chuk <brian_chuk@yahoo.com>
2014-05-24 14:56:46 -0400

    fixed derps. it works now

--- Client.java
+++ Client.java
@@ -37,7 +37,7 @@ public class Client {
 
     }
     //request and return the server IP address
-    private String private String getServerIP() {
+    private String getServerIP() {
         return JOptionPane.showInputDialog(
             frame,
             "Enter IP Address of the Server:",
@@ -73,7 +73,7 @@ public class Client {
         }
     }
      public static void main(String[] args) throws Exception {
-        ChatClient client = new ChatClient();
+        Client client = new Client();
         client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         client.frame.setVisible(true);
         client.run();
